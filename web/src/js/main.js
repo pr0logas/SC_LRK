33918804276c0900fb9cfa02f9e1f2d6b7bdef30
kro <karolisbirgela@gmail.com>
2021-02-24 19:36:37 +0200

    Merging from prologas branch

--- web/src/js/main.js
+++ web/src/js/main.js
@@ -1,4 +1,4 @@
-var CONTRACT_ADDRESS = '0xB55F000ec023B85ab218e4DD281e1a2709620184';
+var CONTRACT_ADDRESS = '0xb572ac670dF22AEa5457EBBdB47df834E9b35BE6';
 var ABI = JSON.parse('[{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"user","type":"address"},{"indexed":true,"internalType":"uint256","name":"date","type":"uint256"},{"indexed":true,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"SubscribeDonatedPeople","type":"event"},{"inputs":[],"name":"kill_sc","outputs":[],"stateMutability":"nonpayable","type":"function"},{"stateMutability":"payable","type":"receive"},{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"CONSTITUTION_RELEASE_BY_UNIXTIME","outputs":[{"internalType":"uint32","name":"","type":"uint32"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"donated_people","outputs":[{"internalType":"address","name":"addr","type":"address"},{"internalType":"uint256","name":"timestamp","type":"uint256"},{"internalType":"uint256","name":"amount","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"get_total_laws_count","outputs":[{"internalType":"uint8","name":"result","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"how_many_people_donated","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"LITHUANIA_FLAG","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"LITHUANIAN_ROOTS_INFO","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"LRK_TITLE","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"OWNER","outputs":[{"internalType":"address payable","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint8","name":"_number","type":"uint8"}],"name":"read_law","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"SC_AUTHORS","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"SMART_CONTRACT_RELEASE_BY_UNIXTIME","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]');
 
 // web3 provider with fallback for old version
@@ -63,10 +63,16 @@ web3.eth.getAccounts(function(err, accounts) {
   });
 })();
 
+
 // Donated People
 (function () {
-  CONTRACT.methods.donated_people(1).call().then( function( donated ) { 
-    console.log(donated);
+  CONTRACT.methods.donated_people(0).call().then( function( donated ) { 
+    robohash = `https://robohash.org/` + donated.addr + `.png?set=set1&size=24x24`
+    $("#user00").attr("src",robohash);
+    $("#user01").text(donated.addr);
+    $("#user02").text(format_date(donated.timestamp));
+    $("#user03").text(format_wei_to_full_num(donated.amount) + ' BNB');
+
   });
 })();
 
@@ -83,7 +89,32 @@ web3.eth.getAccounts(function(err, accounts) {
 // SC Publish date
 (function () {
   CONTRACT.methods.SMART_CONTRACT_RELEASE_BY_UNIXTIME().call().then( function( sc_release_date ) { 
-  let a = new Date(sc_release_date * 1000);
+  $("#screleasedate").html(format_date(sc_release_date));
+  });
+})();
+
+// Donation
+function contribute_to_the_project() {
+  var amount = document.getElementById('amount');
+
+  console.log(amount);
+
+  if (amount <= 0) {
+    alert("Nurodytas tuščias arba netinkamas skaičius?",amount);
+
+  } else {
+    CONTRACT.methods.OWNER().call().then( function(scauthors) {
+      const amount = "0.0004"; 
+      const amountToSend = web3.utils.toWei(amount, "ether"); // Convert to wei value
+      web3.eth.sendTransaction({ from:client_account,to:scauthors, value:amountToSend }).then( function(tx) { ;
+      console.log("Transaction: ", tx); 
+      });
+    });
+  };
+};
+
+function format_date(timestamp) {
+  let a = new Date(timestamp * 1000);
   let months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
   let hours = ['00', '01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23'];
   let year = a.getFullYear();
@@ -93,17 +124,15 @@ web3.eth.getAccounts(function(err, accounts) {
   let min = a.getMinutes();
   let sec = a.getSeconds();
   let time = year + '-' + month + '-' + day + ' ' + hour + ':' + min + ':' + sec ;
-  $("#screleasedate").html(time);
-  });
-})();
+  return time;
+}
 
-// Donation
-function contribute_to_the_project() {
-  CONTRACT.methods.OWNER().call().then( function(scauthors) {
-    const amount = "0.0005"; 
-    const amountToSend = web3.utils.toWei(amount, "ether"); // Convert to wei value
-    web3.eth.sendTransaction({ from:client_account,to:scauthors, value:amountToSend }).then( function(tx) { ;
-    console.log("Transaction: ", tx); 
-    });
-  });
-};
+function format_wei_to_full_num(wei) {
+  let kwei = wei / 1000;
+  let mwei = kwei / 1000;
+  let gwei = mwei / 1000;
+  let szabo = gwei / 1000;
+  let finney = szabo / 1000;
+  let full_num = finney / 1000; // 1 ETH or BNB
+  return full_num;
+}
