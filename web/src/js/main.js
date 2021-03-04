cb176960500ca86689b6aa4ea099605899305f36
pr0logas <t.andriekus@gmail.com>
2021-03-04 19:59:02 +0200

    Date fix

--- web/src/js/main.js
+++ web/src/js/main.js
@@ -229,13 +229,14 @@ function format_date(timestamp) {
   let a = new Date(timestamp * 1000);
   let months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
   let hours = ['00', '01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23'];
+  let days = ['01','02','03','04','05','06','07','08','09','10','12','13','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31']
   let year = a.getFullYear();
   let month = months[a.getMonth()];
-  let day = a.getDate();
+  let day = days[a.getDate()];
   let hour = hours[a.getHours()];
   let min = a.getMinutes();
   let sec = a.getSeconds();
-  let time = year + '-' + month + '-' + day + ' ' + hour + ':' + min + ':' + sec ;
+  let time = year + '-' + month + '-' + day + ' ' + hour + ':' + min + ':' + sec;
   return time;
 }
 
