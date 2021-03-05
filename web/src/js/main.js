b4e9b99b721481c0ccbbd77ada49ac45b3c53a5b
pr0logas <t.andriekus@gmail.com>
2021-03-05 21:42:35 +0200

    Change msg

--- web/src/js/main.js
+++ web/src/js/main.js
@@ -268,7 +268,7 @@ function contribute_to_the_project() {
     }
 
   } else {
-    alert("Jūs neturite susidiegę 'Metamask' įskiepio arba naudojate ne 'BSC blockchain'")
+    alert("Jūs neturite susidiegę 'Metamask' įskiepio arba naudojate ne 'Binance Smart Chain'")
   }
 }
 
