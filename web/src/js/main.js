70c6c96ca55a41604adc03fde80f5f2e4584a62c
pr0logas <t.andriekus@gmail.com>
2021-03-11 00:57:58 +0200

    fallback seeds

--- web/src/js/main.js
+++ web/src/js/main.js
@@ -3,6 +3,18 @@ const ABI = JSON.parse('[{"inputs":[],"stateMutability":"nonpayable","type":"con
 const NETWORK_ID = 56; // BNB network id;
 const PROVIDERS = [
 'https://bsc-dataseed.binance.org',
+'https://bsc-dataseed1.defibit.io',
+'https://bsc-dataseed1.ninicoin.io',
+'https://bsc-dataseed2.defibit.io',
+'https://bsc-dataseed3.defibit.io',
+'https://bsc-dataseed4.defibit.io',
+'https://bsc-dataseed2.ninicoin.io',
+'https://bsc-dataseed3.ninicoin.io',
+'https://bsc-dataseed4.ninicoin.io',
+'https://bsc-dataseed1.binance.org',
+'https://bsc-dataseed2.binance.org',
+'https://bsc-dataseed3.binance.org',
+'https://bsc-dataseed4.binance.org',
 'seed.lietuvos-respublikos-konstitucija.net'
 ];
 var defined_user_web3;
